86c08fa0347b6ee287906c2d71a4ac717747d219
MintNiphada <niphada.y@kkumail.com>
2026-01-22 01:44:44 +0700

    use folder name moodmood-project

---
+++ apps/moodmood-project/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'moodmood-project',
+  webDir: 'dist'
+};
+
+export default config;
