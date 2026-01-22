d0e2ca2b40cb6b4feebfca1cf95c0ffcdf2a08f0
Saw4nya <sawanya.si@kkumail.com>
2026-01-22 17:43:30 +0700

    เมนู Tabs + Routing

--- apps/moodmood-project/src/pages/Tabs.tsx
+++ apps/moodmood-project/src/pages/Tabs.tsx
@@ -4,18 +4,19 @@ import {
   IonTabBar,
   IonTabButton,
   IonIcon,
-  IonLabel
+  IonLabel,
+  IonImg
 } from "@ionic/react";
 import { Route, Redirect } from "react-router-dom";
 import "./Tabs.css";
 import {
   chatbubbleOutline,
   calendarOutline,
-  happyOutline,
   barChartOutline,
   walkOutline
 } from "ionicons/icons";
 
+import duckOkay from "../assets/Okay.svg";
 import Reflection from "./Reflection";
 import Calendar from "./Calendar";
 import Home from "./Home";
@@ -28,14 +29,14 @@ const Tabs: React.FC = () => {
       <IonRouterOutlet>
         <Route exact path="/tabs/mood" component={Reflection} />
         <Route exact path="/tabs/calendar" component={Calendar} />
-        <Route exact path="/tabs/duck" component={Home} />
+        <Route exact path="/tabs/home" component={Home} />
         <Route exact path="/tabs/insight" component={Insight} />
         <Route exact path="/tabs/activity" component={Activity} />
 
-        <Redirect exact from="/tabs" to="/tabs/duck" />
+        <Redirect exact from="/tabs" to="/tabs/home" />
       </IonRouterOutlet>
 
-      <IonTabBar slot="bottom">
+      <IonTabBar slot="bottom" mode = "ios">
         <IonTabButton tab="mood" href="/tabs/mood">
           <IonIcon icon={chatbubbleOutline} />
           <IonLabel>คำถามสะท้อนใจ</IonLabel>
@@ -46,10 +47,11 @@ const Tabs: React.FC = () => {
           <IonLabel>ปฏิทิน</IonLabel>
         </IonTabButton>
 
-        <IonTabButton tab="duck" href="/tabs/duck">
-          <IonIcon icon={happyOutline} />
+        <IonTabButton tab="home" href="/tabs/home">
+          <IonIcon src={duckOkay} className="duck-tab-icon" />
           <IonLabel>หน้าแรก</IonLabel>
         </IonTabButton>
+        
 
         <IonTabButton tab="insight" href="/tabs/insight">
           <IonIcon icon={barChartOutline} />
