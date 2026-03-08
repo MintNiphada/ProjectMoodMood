9eb5584818f42b2d07136123ae5d75a1055cf01f
Saw4nya <sawanya.si@kkumail.com>
2026-03-09 01:55:49 +0700

    Updates

--- assets/ios.transition-legacy-CkMX-mdl.js
+++ assets/ios.transition-legacy-CkMX-mdl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BrChVjME.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BHNSGvmO.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
