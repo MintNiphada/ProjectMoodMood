ca803524268bca4b87e796e718198b30117b2a78
MintNiphada <niphada.y@kkumail.com>
2026-03-09 15:13:10 +0700

    Updates

--- assets/md.transition-legacy-eP2E4Lbw.js
+++ assets/md.transition-legacy-eP2E4Lbw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CuSlFe__.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Na4JgwgX.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
